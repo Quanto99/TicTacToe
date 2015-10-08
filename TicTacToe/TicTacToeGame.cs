a323541f48b01f43c470221cf4b5914da3dfdca2
Quanto99 <scottstavran@comcast.net>
2015-10-08 18:19:53 -0400

    Initial checkin

---
+++ TicTacToe/TicTacToeGame.cs
@@ -0,0 +1,19 @@
+﻿#define CSHARP2
+
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+#pragma warning disable 1030    // disable user defined warnings
+
+namespace TicTacToe
+{
+    class TicTacToeGame
+    {
+        static void Main()
+        {
+        }
+    }
+}
